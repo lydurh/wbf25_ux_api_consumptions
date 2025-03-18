8f66a8d20206054a22355f2998880abf0c945f90
lydur <lydur.hafsteinsson@gmail.com>
2025-03-18 23:06:47 +0100

    got apis to work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
-import { NOW_PLAYING, POPULAR, TOP_RATED, UPCOMING  } from "./script.js";
+import { fetchNowPlaying } from "./info.js";
 
-NOW_PLAYING();
-POPULAR();
-TOP_RATED();
-UPCOMING();
+
+fetchNowPlaying()
+.then (data => {
+  const movieList = document.createDocumentFragment();
+  data.results.forEach(movie => {
+    const card = document.querySelector('#now-playing-card').content.cloneNode(true);
+
+    card.querySelector('h2').innerText = movie.title;
+
+    card.querySelector('#description').innerText = movie.overview;
+
+    const poster = card.querySelector('img');
+    poster.setAttribute('src', movie.poster_path);
+    poster.setAttribute('alt', movie.original_title);
+
+    card.querySelector('#small-title').innerText = movie.original_title;
+
+    card.querySelector('#release_date').innerText = movie.release_date;
+
+  movieList.append(card);
+    
+    
+  });
+  document.querySelector('#upcoming-list').appendChild(movieList);
+})
+
+
+
